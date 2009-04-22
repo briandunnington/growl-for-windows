1d13e869473ddd13919e55a1f0824afa0d59b410
briandunnington <briandunnington@1e52b63b-b251-0410-a6bd-efb3cb96b38b>
2009-04-22 01:30:28 +0000

    Beta7 release

--- Growl/Growl/Properties/AssemblyInfo.cs
+++ Growl/Growl/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 //      Revision
 //
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.6")]
-[assembly: AssemblyInformationalVersion("2.0b6")]
+[assembly: AssemblyFileVersion("2.0.0.7")]
+[assembly: AssemblyInformationalVersion("2.0b7")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
